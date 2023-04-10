fc35ba3cfeb980c665324e50d7c278877eb4212d
zhiyongm <30741108+zhiyongm@users.noreply.github.com>
2023-04-10 21:04:06 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -166,14 +166,16 @@ function _Home() {
 
   return (
     
-<script>
-{var _hmt = _hmt || [];
+ <script type="text/javascript">
+{`
+  var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?e26881914caf042f0274e79a4f21fd5a";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
-})();}
+})();
+`}
 </script>
 
 
